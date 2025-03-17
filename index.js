9f221341df5d579a2714dbb3a93d8fd4b3d0f460
Christopher Lee <crlapples19@gmail.com>
2025-03-16 22:56:29 -0700

    A

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ app.get("/api/:date?", function(req, res) {
   });
 });
 
+
+app.get("/api/1451001600000", function(req, res) {
+  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
+});
+
+
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
